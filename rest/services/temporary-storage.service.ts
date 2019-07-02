d04bbc268328537fa8b63a197390ef54af50f677
tsimon <simon@edu-sharing.net>
2019-07-02 10:56:54 +0200

    fix:collection new styling improved for material

--- rest/services/temporary-storage.service.ts
+++ rest/services/temporary-storage.service.ts
@@ -7,26 +7,27 @@ import {Node} from "../data-object"
  */
 @Injectable()
 export class TemporaryStorageService {
-  public static APPLY_TO_LMS_PARAMETER_NODE ="apply_to_lms_node";
-  public static NODE_RENDER_PARAMETER_OPTIONS = "node_render_options";
-  public static NODE_RENDER_PARAMETER_LIST = "node_render_list";
-  public static NODE_RENDER_PARAMETER_ORIGIN = "node_render_origin";
-  public static COLLECTION_ADD_NODES = "collection_add_nodes";
-  public static SESSION_INFO = "SESSION_INFO";
-  public static LIST_DRAG_DATA = "list_drag";
-  public static MAIN_NAV_BUTTONS = "main_nav_buttons";
+    public static APPLY_TO_LMS_PARAMETER_NODE ="apply_to_lms_node";
+    public static NODE_RENDER_PARAMETER_OPTIONS = "node_render_options";
+    public static NODE_RENDER_PARAMETER_LIST = "node_render_list";
+    public static NODE_RENDER_PARAMETER_ORIGIN = "node_render_origin";
+    public static COLLECTION_ADD_NODES = "collection_add_nodes";
+    public static SESSION_INFO = "SESSION_INFO";
+    public static LIST_DRAG_DATA = "list_drag";
+    public static OPTION_HIDE_MAINNAV: "option-hide_mainnav";
 
-  private data : any={};
+    public static MAIN_NAV_BUTTONS = "main_nav_buttons";
+    private data : any={};
 
-  constructor() {}
-  public get(name : string,defaultValue:any = null){
-    if(this.data[name]!=null)
-      return this.data[name];
-    return defaultValue;
-  }
-  public set(name : string, value : any){
-    this.data[name]=value;
-  }
+    constructor() {}
+    public get(name : string,defaultValue:any = null){
+        if(this.data[name]!=null)
+            return this.data[name];
+        return defaultValue;
+    }
+    public set(name : string, value : any){
+        this.data[name]=value;
+    }
 
     /**
      * same as get, but will remove the value after fetching it
@@ -34,14 +35,14 @@ export class TemporaryStorageService {
      * @param defaultValue
      * @returns {any}
      */
-  public pop(name : string,defaultValue:any = null){
-     let value=this.get(name,defaultValue);
-     this.remove(name);
-     return value;
-  }
-  public remove(name : string) {
-    this.data[name]=null;
-  }
+    public pop(name : string,defaultValue:any = null){
+        let value=this.get(name,defaultValue);
+        this.remove(name);
+        return value;
+    }
+    public remove(name : string) {
+        this.data[name]=null;
+    }
 }
 export interface ClipboardObject {
     nodes: Node[];
