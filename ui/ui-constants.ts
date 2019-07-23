26f7dbdce93e41baf4f2d386eb83d7f19f61e581
tsimon <simon@edu-sharing.net>
2019-07-23 17:28:12 +0200

    feat:responsive modal dialogs concept

--- ui/ui-constants.ts
+++ ui/ui-constants.ts
@@ -3,6 +3,7 @@ export class UIConstants{
   // also declared in scss!
   public static MOBILE_WIDTH = 700;
   public static MOBILE_HEIGHT = 750;
+  public static MOBILE_HEIGHT_WITH_KEYBOARD = 550;
   public static MOBILE_STAGE = 100;
   public static MOBILE_TAB_SWITCH_WIDTH = UIConstants.MOBILE_WIDTH + UIConstants.MOBILE_STAGE*2;
   public static MEDIA_QUERY_MIN_WIDTH="min-width";
