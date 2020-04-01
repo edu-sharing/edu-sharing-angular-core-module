4992a0fdb6a6ecd23eb2953267a3593b2c3e46c7
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-04-01 15:55:33 +0200

    build: fix function types in dialog-button.ts

--- ui/dialog-button.ts
+++ ui/dialog-button.ts
@@ -3,36 +3,36 @@ import {OptionItem} from '../../core-ui-module/option-item';
 export class DialogButton {
     public disabled=false;
 
-    public static getOkCancel(cancel : Function,ok : Function) : DialogButton[]{
+    public static getOkCancel(cancel : () => void,ok : () => void) : DialogButton[]{
         return [
             new DialogButton("CANCEL",DialogButton.TYPE_CANCEL,cancel),
             new DialogButton("OK",DialogButton.TYPE_PRIMARY,ok),
         ];
     }
-    public static getOk(ok : Function) : DialogButton[]{
+    public static getOk(ok : () => void) : DialogButton[]{
         return DialogButton.getSingleButton("OK",ok);
     }
-    public static getCancel(cancel : Function) : DialogButton[]{
+    public static getCancel(cancel : () => void) : DialogButton[]{
         return DialogButton.getSingleButton("CANCEL",cancel,DialogButton.TYPE_CANCEL);
     }
-    public static getSingleButton(label:string,ok : Function,type = DialogButton.TYPE_PRIMARY) : DialogButton[]{
+    public static getSingleButton(label:string,ok : () => void,type = DialogButton.TYPE_PRIMARY) : DialogButton[]{
         return [
             new DialogButton(label,type,ok),
         ];
     }
-    public static getYesNo(no : Function,yes : Function) : DialogButton[]{
+    public static getYesNo(no : () => void,yes : () => void) : DialogButton[]{
         return [
             new DialogButton("NO",DialogButton.TYPE_CANCEL,no),
             new DialogButton("YES",DialogButton.TYPE_PRIMARY,yes),
         ];
     }
-    public static getNextCancel(cancel : Function,next : Function) : DialogButton[]{
+    public static getNextCancel(cancel : () => void,next : () => void) : DialogButton[]{
         return [
             new DialogButton("CANCEL",DialogButton.TYPE_CANCEL,cancel),
             new DialogButton("NEXT",DialogButton.TYPE_PRIMARY,next),
         ];
     }
-    public static getSaveCancel(cancel : Function,save : Function) : DialogButton[]{
+    public static getSaveCancel(cancel : () => void,save : () => void) : DialogButton[]{
         return [
             new DialogButton("CANCEL",DialogButton.TYPE_CANCEL,cancel),
             new DialogButton("SAVE",DialogButton.TYPE_PRIMARY,save),
@@ -53,6 +53,6 @@ export class DialogButton {
      * @param type the button type, use one of the constants
      * @param callback A function callback when this option is choosen.
      */
-    constructor(public name: string,public type : number, public callback: Function) {
+    constructor(public name: string,public type : number, public callback: () => void) {
     }
 }
