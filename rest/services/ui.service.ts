5e6992a0a718e1060e233a51c1ad4b5352f26a48
Torsten Simon <simon@edu-sharing.net>
2020-05-26 12:43:38 +0200

    ui service add logout

--- rest/services/ui.service.ts
+++ rest/services/ui.service.ts
@@ -1,6 +1,11 @@
-
-import {HostListener, Injectable} from "@angular/core";
+import {Injectable} from "@angular/core";
 import {BridgeService} from "../../../core-bridge-module/bridge.service";
+import {HttpClient} from "@angular/common/http";
+import {RestConnectorService} from "./rest-connector.service";
+import {Observable, Observer} from "rxjs";
+import {RestConstants} from "../rest-constants";
+import {MessageType} from "../../ui/message-type";
+
 @Injectable()
 export class UIService {
   /** Returns true if the current sessions seems to be running on a mobile device
@@ -11,12 +16,12 @@ export class UIService {
       return true;
     // http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
     if( navigator.userAgent.match(/Android/i)
-      || navigator.userAgent.match(/webOS/i)
-      || navigator.userAgent.match(/iPhone/i)
-      || navigator.userAgent.match(/iPad/i)
-      || navigator.userAgent.match(/iPod/i)
-      || navigator.userAgent.match(/BlackBerry/i)
-      || navigator.userAgent.match(/Windows Phone/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
     ){
       return true;
     }
@@ -33,7 +38,11 @@ export class UIService {
   public isShiftCmd() {
     return this.shiftCmd;
   }
-  constructor(private bridge : BridgeService) {
+  constructor(
+      private bridge : BridgeService,
+      private http: HttpClient,
+      private connector: RestConnectorService,
+  ) {
     // HostListener not working, so use window
     window.addEventListener('keydown', (event) => {
       if (event.key == 'Shift') {
@@ -54,11 +63,71 @@ export class UIService {
     });
   }
 
-    hideKeyboardIfMobile() {
-        if(this.isMobile()) {
-            try {
-                (document.activeElement as any).blur();
-            }catch(e){console.warn(e);}
-        }
+  hideKeyboardIfMobile() {
+    if(this.isMobile()) {
+      try {
+        (document.activeElement as any).blur();
+      }catch(e){console.warn(e);}
     }
+  }
+  public handleLogout() {
+    return new Observable<void>((observer: Observer<void>) => {
+
+      this.connector.getConfigurationService().getAll().subscribe((config) => {
+        if (this.bridge.isRunningCordova()) {
+          this.connector.logout().subscribe(() => {
+            this.bridge.getCordova().restartCordova();
+          });
+          return;
+        }
+        if (config.logout) {
+          const sessionData = this.connector.getCurrentLogin();
+          if (config.logout.ajax) {
+            this.http.get(config.logout.url).subscribe(
+                () => {
+                  if (config.logout.destroySession) {
+                    this.connector.logout().subscribe(response => {
+                      observer.next();
+                      observer.complete();
+                    });
+                    return;
+                  }
+                  observer.next();
+                  observer.complete();
+                },
+                (error: any) => {
+                  this.bridge.showTemporaryMessage(MessageType.error, null, null, null, error);
+                },
+            );
+          } else {
+            if (config.logout.destroySession) {
+              this.connector.logout().subscribe(response => {
+                if (
+                    sessionData.currentScope ===
+                    RestConstants.SAFE_SCOPE
+                ) {
+                  observer.next();
+                  observer.complete();
+                } else {
+                  window.location.href = config.logout.url;
+                }
+              });
+            } else {
+              if (sessionData.currentScope === RestConstants.SAFE_SCOPE) {
+                observer.next();
+                observer.complete();
+              } else {
+                window.location.href = config.logout.url;
+              }
+            }
+          }
+        } else {
+          this.connector.logout().subscribe(response => {
+            observer.next();
+            observer.complete();
+          });
+        }
+      });
+    });
+  }
 }
