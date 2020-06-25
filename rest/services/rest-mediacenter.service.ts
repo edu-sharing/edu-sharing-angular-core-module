f65bdd6dd2748581a9887b66f15359d643e37f7f
Torsten Simon <simon@edu-sharing.net>
2020-06-25 11:38:43 +0200

    feat:mediacenter add delete endpoint

--- rest/services/rest-mediacenter.service.ts
+++ rest/services/rest-mediacenter.service.ts
@@ -1,49 +1,55 @@
-import {Injectable} from "@angular/core";
-import {RestConstants} from "../rest-constants";
-import {RestConnectorService} from "./rest-connector.service";
-import {AbstractRestService} from "./abstract-rest-service";
-import {IamGroup, Mediacenter, Node, NodeList} from '../data-object';
+import {Injectable} from '@angular/core';
+import {RestConstants} from '../rest-constants';
+import {RestConnectorService} from './rest-connector.service';
+import {AbstractRestService} from './abstract-rest-service';
+import {IamGroup, Mediacenter, MediacenterProfile, Node, NodeList} from '../data-object';
 import {RestHelper} from '../rest-helper';
 
 @Injectable()
-export class RestMediacenterService extends AbstractRestService{
+export class RestMediacenterService extends AbstractRestService {
   // @TODO: declare the mediacenter type when it is finalized in backend
   constructor(connector : RestConnectorService) {
       super(connector);
   }
-  public getMediacenters(repository = RestConstants.HOME_REPOSITORY){
-    let query = this.connector.createUrl("mediacenter/:version/mediacenter/:repository", repository);
+  public getMediacenters(repository = RestConstants.HOME_REPOSITORY) {
+    const query = this.connector.createUrl('mediacenter/:version/mediacenter/:repository', repository);
     return this.connector.get<Mediacenter[]>(query, this.connector.getRequestOptions());
   }
-  public addMediacenter(mediacenter:string,profile:any,repository = RestConstants.HOME_REPOSITORY){
-    let query = this.connector.createUrl("mediacenter/:version/mediacenter/:repository/:mediacenter", repository,[
-      [":mediacenter",mediacenter]
+  public addMediacenter(mediacenter:string,profile:any,repository = RestConstants.HOME_REPOSITORY) {
+    const query = this.connector.createUrl('mediacenter/:version/mediacenter/:repository/:mediacenter', repository,[
+      [':mediacenter',mediacenter]
     ]);
-    return this.connector.post<Mediacenter>(query,JSON.stringify(profile),this.connector.getRequestOptions());
+    return this.connector.post<Mediacenter>(query, JSON.stringify(profile), this.connector.getRequestOptions());
   }
-  public editMediacenter(mediacenter:string,profile:any,repository = RestConstants.HOME_REPOSITORY){
-    let query = this.connector.createUrl("mediacenter/:version/mediacenter/:repository/:mediacenter", repository,[
-        [":mediacenter",mediacenter]
+  public editMediacenter(mediacenter:string,profile: MediacenterProfile,repository = RestConstants.HOME_REPOSITORY) {
+    const query = this.connector.createUrl('mediacenter/:version/mediacenter/:repository/:mediacenter', repository,[
+        [':mediacenter',mediacenter]
     ]);
-    return this.connector.put<Mediacenter>(query,JSON.stringify(profile),this.connector.getRequestOptions());
+    return this.connector.put<Mediacenter>(query, JSON.stringify(profile), this.connector.getRequestOptions());
   }
-  public addManagedGroup(mediacenter:string,group:string,repository = RestConstants.HOME_REPOSITORY){
-    let query = this.connector.createUrl("mediacenter/:version/mediacenter/:repository/:mediacenter/manages/:group", repository,[
-      [":mediacenter",mediacenter],
-      [":group",group]
+  public deleteMediacenter(mediacenter:string, repository = RestConstants.HOME_REPOSITORY) {
+      const query = this.connector.createUrl('mediacenter/:version/mediacenter/:repository/:mediacenter', repository,[
+          [':mediacenter',mediacenter]
+      ]);
+      return this.connector.delete<void>(query, this.connector.getRequestOptions());
+  }
+  public addManagedGroup(mediacenter:string,group:string,repository = RestConstants.HOME_REPOSITORY) {
+    const query = this.connector.createUrl('mediacenter/:version/mediacenter/:repository/:mediacenter/manages/:group', repository,[
+      [':mediacenter',mediacenter],
+      [':group',group]
     ]);
     return this.connector.put<IamGroup[]>(query,null,this.connector.getRequestOptions());
   }
-  public removeManagedGroup(mediacenter:string,group:string,repository = RestConstants.HOME_REPOSITORY){
-    let query = this.connector.createUrl("mediacenter/:version/mediacenter/:repository/:mediacenter/manages/:group", repository,[
-      [":mediacenter",mediacenter],
-      [":group",group]
+  public removeManagedGroup(mediacenter:string,group:string,repository = RestConstants.HOME_REPOSITORY) {
+    const query = this.connector.createUrl('mediacenter/:version/mediacenter/:repository/:mediacenter/manages/:group', repository,[
+      [':mediacenter',mediacenter],
+      [':group',group]
     ]);
-    return this.connector.delete<IamGroup[]>(query,this.connector.getRequestOptions());
+    return this.connector.delete<IamGroup[]>(query, this.connector.getRequestOptions());
   }
-  public getManagedGroups(mediacenter:string,repository = RestConstants.HOME_REPOSITORY){
-    let query = this.connector.createUrl("mediacenter/:version/mediacenter/:repository/:mediacenter/manages", repository,[
-      [":mediacenter",mediacenter]
+  public getManagedGroups(mediacenter:string,repository = RestConstants.HOME_REPOSITORY) {
+    const query = this.connector.createUrl('mediacenter/:version/mediacenter/:repository/:mediacenter/manages', repository,[
+      [':mediacenter',mediacenter]
     ]);
     return this.connector.get<IamGroup[]>(query,this.connector.getRequestOptions());
   }
@@ -59,38 +65,38 @@ export class RestMediacenterService extends AbstractRestService{
                           facettes : string[] = [],
                           request : any = null) {
       console.log(searchWord);
-        let query=this.connector.createUrlNoEscape("mediacenter/:version/mediacenter/:repository/:mediacenter/licenses?:searchword&:facettes&:request",repository,
+        const query=this.connector.createUrlNoEscape('mediacenter/:version/mediacenter/:repository/:mediacenter/licenses?:searchword&:facettes&:request',repository,
             [
-                [":mediacenter",encodeURIComponent(mediacenter)],
-                [":searchword",RestHelper.getQueryString("searchword", [searchWord])],
-                [":facettes",RestHelper.getQueryStringForList("facettes",facettes)],
-                [":request",this.connector.createRequestString(request)]
+                [':mediacenter',encodeURIComponent(mediacenter)],
+                [':searchword',RestHelper.getQueryString('searchword', [searchWord])],
+                [':facettes',RestHelper.getQueryStringForList('facettes',facettes)],
+                [':request',this.connector.createRequestString(request)]
             ]);
-        return this.connector.post<NodeList>(query,"",this.connector.getRequestOptions());
+        return this.connector.post<NodeList>(query,'',this.connector.getRequestOptions());
 
     }
 
-    public importMediacenters(file : File){
-        let query=this.connector.createUrl("mediacenter/:version/import/mediacenters",null);
-        let options=this.connector.getRequestOptions();
+    public importMediacenters(file : File) {
+        const query=this.connector.createUrl('mediacenter/:version/import/mediacenters',null);
+        const options=this.connector.getRequestOptions();
 
-        return this.connector.sendDataViaXHR(query,file,"POST","mediacenters")
+        return this.connector.sendDataViaXHR(query,file,'POST','mediacenters')
             .map((response:XMLHttpRequest) => {return JSON.parse(response.response)});
     }
 
-    public importOrganisations(file : File){
-        let query=this.connector.createUrl("mediacenter/:version/import/organisations",null);
-        let options=this.connector.getRequestOptions();
+    public importOrganisations(file : File) {
+        const query=this.connector.createUrl('mediacenter/:version/import/organisations',null);
+        const options=this.connector.getRequestOptions();
 
-        return this.connector.sendDataViaXHR(query,file,"POST","organisations")
+        return this.connector.sendDataViaXHR(query,file,'POST','organisations')
             .map((response:XMLHttpRequest) => {return JSON.parse(response.response)});
     }
 
-    public importMcOrgConnections(file : File, removeSchoolsFromMC : boolean){
-        let query=this.connector.createUrl("mediacenter/:version/import/mc_org?removeSchoolsFromMC=" + removeSchoolsFromMC,null);
-        let options=this.connector.getRequestOptions();
+    public importMcOrgConnections(file : File, removeSchoolsFromMC : boolean) {
+        const query=this.connector.createUrl('mediacenter/:version/import/mc_org?removeSchoolsFromMC=' + removeSchoolsFromMC,null);
+        const options=this.connector.getRequestOptions();
 
-        return this.connector.sendDataViaXHR(query,file,"POST","mcOrgs")
+        return this.connector.sendDataViaXHR(query,file,'POST','mcOrgs')
             .map((response:XMLHttpRequest) => {return JSON.parse(response.response)});
     }
 }
