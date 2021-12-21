dbe316ca7d1f40b02972c2a91982c00f3db6892b
Daniel Rudolph <rudolph@edu-sharing.net>
2021-12-21 16:04:35 +0100

    fix: add authorityscope for lucene export as csv

--- rest/services/rest-admin.service.ts
+++ rest/services/rest-admin.service.ts
@@ -220,11 +220,12 @@ export class RestAdminService extends AbstractRestService{
         ]);
         return this.connector.get<NodeListElastic>(query,this.connector.getRequestOptions());
     }
-  public exportLucene(lucene:string,store:string,properties:string[]){
+  public exportLucene(lucene:string,store:string,properties:string[],authorities:string[]){
     let query=this.connector.createUrlNoEscape("admin/:version/lucene/export?query=:lucene&store=:store&:properties",null,[
         [":lucene",encodeURIComponent(lucene)],
         [":store",encodeURIComponent(store)],
         [":properties",RestHelper.getQueryStringForList("properties",properties)],
+        [":authorities",RestHelper.getQueryStringForList("authorityScope",authorities)]
     ]);
     return this.connector.get<any>(query,this.connector.getRequestOptions());
   }
