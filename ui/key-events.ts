e45ce49ed1e779dec35c2004a35ecf22de698db0
Torsten Simon <simon@edu-sharing.net>
2022-04-13 17:56:43 +0200

    fix:modal card stop event propagation to background elements

--- ui/key-events.ts
+++ ui/key-events.ts
@@ -1,6 +1,17 @@
+import {ElementRef} from '@angular/core';
 
 export class KeyEvents{
-  public static eventFromInputField(event:any) {
-    return event.srcElement && (event.srcElement.minLength || event.srcElement.maxLength);
-  }
+    public static eventFromInputField(event:any) {
+        return event.srcElement && (event.srcElement.minLength || event.srcElement.maxLength);
+    }
+    static isChildEvent(event: Event, parent: ElementRef<Element>) {
+        let target: HTMLElement = event?.target as HTMLElement;
+        while(target) {
+            if(target === parent.nativeElement) {
+                return true;
+            }
+            target = target.parentElement;
+        }
+        return false;
+    }
 }
