fe78a8822b6a83ef0b5f33115cd5429f90957848
Torsten Simon <simon@edu-sharing.net>
2024-04-05 09:57:43 +0200

    fix:csv obey "0" numbers

--- csv.helper.ts
+++ csv.helper.ts
@@ -26,7 +26,9 @@ export class CsvHelper {
                     line.push(d[h]);
                 }
             }
-            csv += line.map((l) => '"' + (l ? (l + '').replace(/"/g, '""') : '') + '"').join(';');
+            csv += line
+                .map((l) => '"' + (l !== null ? (l + '').replace(/"/g, '""') : '') + '"')
+                .join(';');
         }
         return csv;
     }
