cf1b1bf2bfa57c2ec64da246bcb0e828e98bd0e8
Torsten Simon <simon@edu-sharing.net>
2025-02-23 22:04:05 +0100

    fix:move constant

--- rest/rest-constants.ts
+++ rest/rest-constants.ts
@@ -285,7 +285,6 @@ export class RestConstants extends RestConstantsBase {
     public static HTTP_FORBIDDEN = 403;
     public static HTTP_NOT_FOUND = 404;
     public static HOME_APPLICATION_XML = 'homeApplication.properties.xml';
-    public static NODE_VERSION_CURRENT = '-1';
     public static PRIMARY_SEARCH_CRITERIA = 'ngsearchword';
     public static DISPLAYNAME_SUFFIX = '_DISPLAYNAME';
     public static SAVED_SEARCH = '-saved_search-';
