0fb4c03ebdc30fb957d5517f7dc793a80bfec3c1
Sebastian Wollner <wollner@edu-sharing.net>
2025-10-27 13:51:52 +0100

    feat: add PrimaryLogin type import to data-object.ts

--- rest/data-object.ts
+++ rest/data-object.ts
@@ -19,6 +19,7 @@ import {
 } from 'ngx-edu-sharing-api';
 
 import { ListItem, VCard } from 'ngx-edu-sharing-ui';
+import { PrimaryLogin } from '../../../../projects/edu-sharing-api/src/lib/api/models/primary-login';
 
 export { Connector, ConnectorList } from 'ngx-edu-sharing-api';
 export { GroupProfile } from 'ngx-edu-sharing-api';
