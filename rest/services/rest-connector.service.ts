fdfc7f90c0d9367d40c14d1a18dde8fae8f047b6
Torsten Simon <simon@edu-sharing.net>
2025-11-10 14:26:21 +0100

    fix:special file type issues for body stream exhausted on ios

--- rest/services/rest-connector.service.ts
+++ rest/services/rest-connector.service.ts
@@ -7,7 +7,7 @@ import {
     ConfigService,
     LoginInfo,
 } from 'ngx-edu-sharing-api';
-import { TemporaryStorageService } from 'ngx-edu-sharing-ui';
+import { TemporaryStorageService, UIService } from 'ngx-edu-sharing-ui';
 import { BehaviorSubject, Observable, Observer, Subject, of } from 'rxjs';
 import { first, switchMap, tap } from 'rxjs/operators';
 import { v4 as uuidv4 } from 'uuid';
@@ -309,7 +309,11 @@ export class RestConnectorService implements OnDestroy {
                 }
                 let formData = new FormData();
                 if (file) {
-                    formData.append(fieldName, file, file.name);
+                    // dirty hack for request body stream exhausted error on some ios formats
+                    if (UIService.isSafari()) {
+                        file = new File([file], file.name, { type: 'application/octet-stream' });
+                    }
+                    formData.append(fieldName, file);
                 }
                 let progress: UploadProgress = { start: new Date().getTime() };
                 xhr.upload.addEventListener('progress', (event: any) => {
