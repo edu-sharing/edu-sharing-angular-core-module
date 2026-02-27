7a79d0d9bf720d9e9dce6c5a9be66487ce3a6d3a
Torsten Simon <simon@edu-sharing.net>
2026-02-27 13:17:53 +0100

    fix:sidebar methodics store last value fix:refactor/migrate session storage to promise

--- rest/services/session-storage.service.ts
+++ rest/services/session-storage.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
-import { combineLatest, EMPTY, merge, Observable, of, Subject, throwError } from 'rxjs';
+import {
+    combineLatest,
+    EMPTY,
+    firstValueFrom,
+    merge,
+    Observable,
+    of,
+    Subject,
+    throwError,
+} from 'rxjs';
 import {
     catchError,
     distinctUntilChanged,
@@ -9,7 +18,6 @@ import {
     shareReplay,
     startWith,
     switchMap,
-    takeUntil,
     tap,
 } from 'rxjs/operators';
 import { RestIamService } from '../../rest/services/rest-iam.service';
@@ -100,8 +108,8 @@ export class SessionStorageService {
      * Gets a current storage value from the backend or browser storage, depending on login state
      * and `store`.
      */
-    get(key: string, fallback: any = null, store = Store.UserProfile): Observable<any> {
-        return this.observe(key, fallback, store).pipe(first());
+    get<T>(key: string, fallback: T = null, store = Store.UserProfile): Promise<T> {
+        return firstValueFrom(this.observe(key, fallback, store));
     }
 
     /**
