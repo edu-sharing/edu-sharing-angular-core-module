69f21693adc670ba7606d3e9cfe84e197fe632fd
Torsten Simon <simon@edu-sharing.net>
2026-06-01 12:18:19 +0200

    feat:enhance 2FA handling with session-based verification (do not resend user/password, only code)

--- rest/services/rest-connector.service.ts
+++ rest/services/rest-connector.service.ts
@@ -193,8 +193,19 @@ export class RestConnectorService implements OnDestroy {
         });
     }
 
-    public login(username: string, password: string, scope?: string, code2Fa?: string) {
-        return this.authenticationApi.login(username, password, scope, code2Fa).pipe(
+    public login(username: string, password: string, scope?: string) {
+        return this.authenticationApi.login(username, password, scope).pipe(
+            tap((loginInfo) => {
+                if (loginInfo.isValidLogin) {
+                    this.event.broadcastEvent(FrameEventsService.EVENT_USER_LOGGED_IN, loginInfo);
+                }
+                this.currentLogin.next(loginInfo);
+            }),
+        );
+    }
+
+    public verify2Fa(code2Fa: string) {
+        return this.authenticationApi.verify2Fa(code2Fa).pipe(
             tap((loginInfo) => {
                 if (loginInfo.isValidLogin) {
                     this.event.broadcastEvent(FrameEventsService.EVENT_USER_LOGGED_IN, loginInfo);
